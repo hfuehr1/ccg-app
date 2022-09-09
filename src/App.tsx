416c2e614906aa1f176dce7839bdd6c1157c62a6
Henrik Führ <hfuehr1@unie-koeln.de>
2022-09-09 09:18:24 +0200

    test

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import BasicMapComponent from './components/BasicMapComponent';
+import BasicNominatimSearch from './components/BasicNominatimSearch';
+import SideDrawer from './components/SideDrawer';
+import ToggleDrawerButton from './components/ToggleDrawerButton';
+
+import './App.less';
+
+export const App: React.FC = (): JSX.Element => {
+  return (
+    <div className="App">
+      <BasicMapComponent />
+      <BasicNominatimSearch />
+      <ToggleDrawerButton />
+      <SideDrawer />
+      <button />
+    </div>
+  );
+};
+
+export default App;
